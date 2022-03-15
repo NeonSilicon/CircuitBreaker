aafb108a57eb093a4aab98f814598a9f8dd2b1f9
Neon Silicon <neonsilicon@gmail.com>
2022-03-15 03:23:05 -0600

    Initial checkin of configuration files.

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ -f "Xcode_Config/DeveloperSettings.xcconfig" ] ; then
+   echo The configuration file already exists.
+   echo Please edit the Xcode_Config/DeveloperSettings.xcconfig file directly.
+   exit 1
+fi
+
+echo Configuring developer credentials and plugin name.
+echo
+echo The answers to these questions will be written to the Xcode configuration
+echo for the project in the Xcode_Config directory.
+echo
+read -p "Press enter/return to begin."
+
+echo What name do you want to use for the application?
+read app_name
+
+echo What is your developer team ID? This can be left blank for unsigned macOS builds.
+echo The information is available at developer.apple.com for registered developers.
+read team_id
+
+echo What is your organization identifier, e.g. com.companyname
+read org_id
+
+echo Creating the Xcode_Config directory.
+mkdir -p Xcode_Config
+
+echo Creating the shared configuration file.
+
+cat <<file >> Xcode_Config/DeveloperSettings.xcconfig
+CODE_SIGN_IDENTITY[sdk=macosx*] = Mac Developer
+CODE_SIGN_IDENTITY[sdk=iphoneos*] = iPhone Developer
+CODE_SIGN_IDENTITY[sdk=iphonesimulator*] = iPhone Developer
+DEVELOPMENT_TEAM = $team_id
+CODE_SIGN_STYLE = Automatic
+ORGANIZATION_IDENTIFIER = $org_id
+DEVELOPER_ENTITLEMENTS = -dev
+PROVISIONING_PROFILE_SPECIFIER =
+PRODUCT_NAME = $app_name
+file
+
+echo Finished configuration.
